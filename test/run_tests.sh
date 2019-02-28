da0db78091e62d4dbd7a248d95a40d5d8ccb525a
Chris Crook <ccrook@linz.govt.nz>
2019-03-01 11:09:35 +1300

    Tidying up test script

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -3,6 +3,7 @@
 # Local configuration
 
 outputdir=output
+filedir=files
 logfile=$outputdir/test_get_gnss_data.log
 script=../bin/get_gnss_data
 cachedir=test_cache
@@ -14,6 +15,8 @@ export PERL5LIB=../lib:$PERL5LIB
 
 mkdir -p $outputdir
 rm -rf $outputdir/*
+rm -rf $filedir
+mkdir -p $filedir
 rm -rf $cachedir
 mkdir -p $cachedir
 
@@ -22,26 +25,67 @@ if [ ! -e $HOME/linz-gnss-credentials ]; then
     exit
 fi
 
+test=$1
+
+if [ -z "$test" ]; then
 $script -l > $outputdir/datatypes.txt
 $script -s > $outputdir/sources.txt
+fi
+
+######################################################
 # Get an FTP site...
+if [ -z "$test" -o "$test" = "ftp" ]; then
 echo "Getting FTP UNAVCO file"
-$script -n UNAVCO -d $outputdir 2018:100 OBS ROB4 > $outputdir/ftp_unavco.log 2>&1
+$script -n UNAVCO -d $filedir 2018:100 OBS ROB4 > $outputdir/ftp_unavco.log 2>&1
+cksum $filedir/* >> $outputdir/ftp_unavco.log
+rm $filedir/*
+fi
+
+######################################################
+if [ -z "$test" -o "$test" = "http" ]; then
 echo "Getting unauthorised HTTP"
-$script -n GGOS -d $outputdir 2018:100 VMF  > $outputdir/http_ggos_2.log 2>&1
-echo "Getting HTTP SOPAC file"
-$script -n SOPAC -d $outputdir 2018:100 OBS MCM4 > $outputdir/http_sopac.log 2>&1
-echo "Getting GNSSArchive file"
-$script -n LINZ-GNSSArchive -d $outputdir 2018:200 OBS GSAL > $outputdir/gnssa_linz.log 2>&1
+$script -n GGOS -d $filedir 2018:100 VMF  > $outputdir/http_ggos_2.log 2>&1
+cksum $filedir/* >> $outputdir/http_ggos_2.log
+rm $filedir/*
+fi
+
+######################################################
+if [ -z "$test" -o "$test" = "httpa" ]; then
+echo "Getting HTTP file (basic authentication)"
+$script -n SOPAC -d $filedir 2018:100 OBS MCM4 > $outputdir/http_sopac.log 2>&1
+cksum $filedir/* >> $outputdir/http_sopac.log
+rm $filedir/*
+fi
+
+######################################################
+if [ -z "$test" -o "$test" = "gnssa" ]; then
+echo "Getting Public GNSSArchive file"
+$script -n LINZ-GNSSArchive -d $filedir 2018:200 OBS AUCK > $outputdir/gnssa_linz.log 2>&1
+cksum $filedir/* >> $outputdir/gnssa_linz.log
+rm $filedir/*
+fi
+
+######################################################
+if [ -z "$test" -o "$test" = "gnssp" ]; then
+echo "Getting Private GNSSArchive file"
+$script -n LINZ-GNSSArchive -d $filedir 2018:200 OBS GSAL > $outputdir/gnssp_linz.log 2>&1
+cksum $filedir/* >> $outputdir/gnssp_linz.log
+rm $filedir/*
+fi
+
+######################################################
+if [ -z "$test" -o "$test" = "cache" ]; then
 echo "Getting file to cache"
 $script -c 2018:200 OBS AUCK > $outputdir/cachefile.log 2>&1
-ls $outputdir >> $outputdir/cachefile.log
+ls $outputdir >> $filedir/cachefile.log
 echo "Getting file from cache"
-$script -d $outputdir 2018:200 OBS AUCK > $outputdir/get_cache_file.log 2>&1
-# Form checksum of output files to reduce output size
-ls $outputdir/*.18* $outputdir/*.H* | xargs cksum > $outputdir/output_files.txt
-rm $outputdir/*.18* $outputdir/*.H*
+$script -d $filedir 2018:200 OBS AUCK > $outputdir/get_cache_file.log 2>&1
+cksum $filedir/* >> $outputdir/get_cache_file.log
+rm $filedir/*
+fi
 
+
+######################################################
 # Remove date reference from log files
 perl -pi -e 's~^20\d{2}/\d{2}/\d{2}\s\d{2}\:\d{2}\:\d{2}~2000/01/01 00:00:00~' $outputdir/*.log
 perl -pi -e 's~/tmp/gnss\w+/~/tmp/gnss/~' $outputdir/*.log
